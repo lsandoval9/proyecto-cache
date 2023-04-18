c08935c281663534821bdf2544167d909ac517f8
lsandoval9 <lizarraga309@gmail.com>
2023-04-18 13:41:07 -0400

    update everything

--- main.cpp
+++ main.cpp
@@ -3,36 +3,46 @@
  * @brief Programa para emular el funcionamiento de una memoria cache para correspondencia directa y asociativa por conjuntos
  * @author Luis Sandoval - 26.781.082
  * @author Gerardo Diaz - TODO
- * @organization Arquitectura del computador - Prof. Jose Canache
-*/
+ * Facultad experimental de ciencias y tecnologia - Universidad de Carabobo
+ * Arquitectura del computador - Prof. Jose Canache
+ */
 
-#ifndef WORD_SIZE
+// constantes
 
-#define WORD_SIZE 32 // Tamaño de la palabra en bits - las palabras usualmente son de 32 bits o 64 bits
-
-#endif
-
+#include "./constants/constants.h"
 
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <unistd.h>
+#include <map>
+#include <bitset>
+#include <fstream>
+
+using namespace std;
 
 // librerias propias
 
 #include "helpers/baseNParser.h"
 #include "lib/setAssociativeCache.h"
-#include "lib/directMappedCache.h"
 
-using namespace std;
+// variables globales
+
+SetAssociativeCache *globalSetAssociativeCache; // Cache para correspondencia por conjuntos de n vias
 
 // prototipos de funciones
 
-void print_menu();
+void printMenu();
 
-void set_values(long &s_block, long &s_cache, long &n_ways);
+void setValues();
 
-void cache_features(long s_block, long s_cache, long n_ways);
+void printFeatures(short option);
+
+void finalizarPrograma();
+
+void imprimirInformacion();
+
+void readBlocksFromFile();
 
 /**
  * Programa para emular el funcionamiento de una memoria cache para correspondencia directa y
@@ -41,64 +51,65 @@ void cache_features(long s_block, long s_cache, long n_ways);
 int main(int argc, char const *argv[])
 {
 
-  short input = 20;
+  int input = -1;
 
-  long s_block;
-
-  long s_cache;
-
-  long n_ways;
-
-  set_values(s_block, s_cache, n_ways);
+  setValues();
+  readBlocksFromFile();
 
   while (input != 0)
   {
 
     cout << endl;
-    print_menu();
+    printMenu();
     cin >> input;
+    cout << endl;
 
     switch (input)
     {
 
-    case 1:
-      cache_features(s_block, s_cache, n_ways);
+    case 1: // Imprimir prestaciones de la cache
+      printFeatures(1);
       break;
     case 2:
-      cout << "Opcion 2";
+      printFeatures(2);
       break;
     case 3:
-      cout << "Opcion 3";
+      readBlocksFromFile();
       break;
-    case 4:
-      set_values(s_block, s_cache, n_ways);
+    case 4: // Reiniciar valores
+      setValues();
       break;
-
-    case 5:
-      cout << "Programa para emular el funcionamiento de una memoria cache para correspondencia directa y asociativa por conjuntos" << endl;
-      cout << "Hecho por Luis Sandoval - 26.781.082" << endl;
-      cout << "Hecho por Gerardo Diaz - TODO" << endl;
-      cout << "Arquitectura del computador - Prof. Jose Canache" << endl;
+    case 5: // imprimir informacion del programa
+      imprimirInformacion();
       break;
-
+    case 0: // Salir
+      finalizarPrograma();
+      break;
     default:
       cout << "¡Opcion invalida!";
     }
+
+    sleep(2);
   }
 
   return 0;
 }
 
-
-
 // FUNCIONES EXTERNAS
 
 /**
  * Funcion para obtener los valores de tamaño de bloque y tamaño de cache
- * @param s_block tamaño del bloque en palabras (referencia)
- * @param s_cache tamaño de la cache en KB (referencia)
+ * @param s_block Tamaño del bloque
+ * @param s_cache Tamaño de la cache
+ * @param n_ways Numero de vias
  */
-void set_values(long &s_block, long &s_cache, long &n_ways) {
+void setValues()
+{
+  long s_block; // Tamaño del bloque en palabras de 32 bits
+
+  long s_cache; // Tamaño de la cache en KBs
+
+  long n_ways; // Numero de vias
 
   cout << "ingrese el tamaño de la memoria cache en KB (potencia de 2): ";
 
@@ -114,54 +125,126 @@ void set_values(long &s_block, long &s_cache, long &n_ways) {
 
   if (s_cache % 2 != 0 && s_block % 2 != 0)
   {
-    throw "¡El tamaño de la memoria cache y el tamaño del bloque deben ser multiplos de 2!";
+    throw std::runtime_error("¡El tamaño de la memoria cache y el tamaño del bloque deben ser multiplos de 2!");
   }
 
   if (s_cache < 1 || s_block < 1)
   {
-    throw "¡El tamaño de la memoria cache y el tamaño del bloque deben ser mayores a 0!";
+    throw std::runtime_error("¡El tamaño de la memoria cache y el tamaño del bloque deben ser mayores a 0!");
   }
 
   if (s_cache % s_block != 0)
   {
-    throw "¡El tamaño de la memoria cache debe ser multiplo del tamaño del bloque!";
+    throw std::runtime_error("¡El tamaño de la memoria cache debe ser multiplo del tamaño del bloque!");
   }
 
   if (n_ways % 2 != 0)
   {
-    throw "¡El numero de vias debe ser multiplo de 2!";
+    throw std::runtime_error("¡El numero de vias debe ser multiplo de 2!");
   }
 
-  if (WORD_SIZE % s_block != 0)
+  long blockInCache = s_cache * 1024 / (s_block * 4);
+
+  cout << "Numero de bloques en la cache12: " << blockInCache << endl;
+
+  if (n_ways > blockInCache)
   {
-    throw "¡El tamaño de la palabra debe ser multiplo del tamaño del bloque!";
+    throw std::runtime_error("¡El numero de vias no puede ser mayor al tamaño de la cache!");
   }
 
+  globalSetAssociativeCache = new SetAssociativeCache(s_block, s_cache, n_ways);
 }
 
 /**
  * Funcion para imprimir el menu de opciones del programa
-*/
-void print_menu()
+ */
+void printMenu()
 {
-  cout << "Ingrese la opcion deseada a continuacion(1,2,3): " << endl;
-  cout << " 1 - ver prestaciones de la cache" << endl;
-  cout << " 2 - cache de correspondencia directa" << endl;
-  cout << " 3 - cache asociativa por conjuntos" << endl;
-  cout << " 4 - asignar bloque de la cache" << endl;
-  cout << " 5 - cambiar valores" << endl;
-  cout << " 6 - acerca del programa" << endl;
+  cout << "Ingrese la opcion deseada a continuacion(1,2,3,...): " << endl;
+  cout << " 1 - Informacion cache de correspondencia directa" << endl;
+  cout << " 2 - Informacion asociativa por conjuntos" << endl;
+  cout << " 3 - NADA" << endl;
+  cout << " 4 - cambiar valores" << endl;
+  cout << " 5 - acerca del programa" << endl;
   cout << " 0 - salir" << endl;
-  cout << "seleccion: ";
+  cout << "seleccion: > ";
 }
 
+/**
+ * Funcion para imprimir las prestaciones de la cache
+ * @param option Opcion de cache a imprimir
+ * @note 1 - Cache de correspondencia directa
+ * @note 2 - Cache de asociativa por conjuntos de N vias
+ */
+void printFeatures(short option)
+{
 
-void cache_features(long s_block, long s_cache, long n_ways) {
+  globalSetAssociativeCache->printFeatures();
+}
 
-  // SetAssociativeCache *cache = new SetAssociativeCache();
+void finalizarPrograma()
+{
 
-  DirectMappedCache *cache = new DirectMappedCache(s_block, s_cache);
+  cout << "\nFin del programa";
 
-  cache->getFeatures();
+  delete globalSetAssociativeCache;
 
+  exit(0);
+}
+
+void imprimirInformacion()
+{
+
+  cout << "Programa para emular el funcionamiento de una memoria cache para correspondencia directa y asociativa por conjuntos" << endl;
+
+  cout << "Autores: " << endl;
+
+  cout << "Luis Sandoval - 26.781.082" << endl;
+
+  cout << "Gerardo Diaz - TODO" << endl;
+
+  cout << "Profesor: Jose Canache" << endl;
+
+  cout << "Universidad de Carabobo" << endl;
+
+  cout << "Facultad Experimental de Ciencias y Tecnologia" << endl;
+
+  cout << "Arquitectura del computador" << endl;
+}
+
+/**
+ * Funcion para guardar un bloque de la memoria principal en la cache
+ * @param input Direccion del bloque a guardar
+ */
+void readBlocksFromFile()
+{
+
+  // Read hex numbers from a file where each number is separated by an endline
+
+  ifstream myFile;
+
+  myFile.open("entradas.in", ios::in);
+
+  if (!myFile.is_open())
+  {
+    throw runtime_error("¡No se pudo abrir el archivo!");
+  }
+
+  while (!myFile.eof())
+  {
+
+    string input;
+
+    // Convert hex to binary
+
+    getline(myFile, input);
+
+    long binaryNumber = BaseNParser::parseHexStringToNumber(input);
+
+    globalSetAssociativeCache->saveBlockInCache(binaryNumber);
+
+    cout << "Se guardo el bloque " << input << " en la cache" << endl;
+  }
+
+  myFile.close();
 }
